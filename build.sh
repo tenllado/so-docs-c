89ccfcefbc2cea50d25833b3abe02b9b6154ad06
Juan Carlos Saez <jcsaezal@ucm.es>
2022-07-21 10:42:01 +0200

    Added pdf version of the assignment sheets

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-documents=('../intro-c.md entorno.html' \
-	'../intro-sistemas.md intro-sistemas.html' )
+documents=('../intro-c.md entorno' \
+	'../intro-sistemas.md intro-sistemas' )
 
 ndocs=${#documents[@]}
 
@@ -8,8 +8,11 @@ for (( i=0 ; $i<$ndocs ; i++ ))
 do
 	items=(${documents[$i]})
 	source_file=${items[0]}
-	target_file=${items[1]}
-	echo "${source_file} --> ${target_file}"
-	pandoc ${source_file} -N --self-contained  --template pandoc-templates/toc-sidebarL-title.html-template.html --toc  -o ${target_file}
+	target_file1=${items[1]}.html
+	target_file2=${items[1]}.pdf
+	echo "${source_file} --> ${target_file1}"
+	pandoc ${source_file} -N --self-contained  --template pandoc-templates/toc-sidebarL-title.html-template.html --toc  -o ${target_file1}
+	echo "${source_file} --> ${target_file2}"
+	pandoc ${source_file} -N --toc  -o ${target_file2}
 done
 
