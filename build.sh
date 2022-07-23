3efea11251dfb6bbb063e40b39d66aba09f45054
Juan Carlos Saez <jcsaezal@ucm.es>
2022-07-23 12:12:37 +0200

    update code-formatting style

--- build.sh
+++ build.sh
@@ -13,16 +13,29 @@ fi
 
 cp -rf ../img .
 
+selected_document=$1
+
+if [ "${selected_document}" != "" ]; then
+	filter_on=1
+else
+	filter_on=0
+fi
+
 for (( i=0 ; $i<$ndocs ; i++ ))
 do
 	items=(${documents[$i]})
 	source_file=${items[0]}
 	target_file1=${items[1]}.html
 	target_file2=${items[1]}.pdf
+
+	if [ ${filter_on} -eq 1 ] && [ "${selected_document}" != "${items[1]}" ]; then
+		continue
+	fi
+
 	echo "${source_file} --> ${target_file1}"
-	pandoc ${source_file} -N --self-contained  --template pandoc-templates/toc-sidebarL-title.html-template.html --toc  -o ${target_file1}
+	pandoc ${source_file} -N --self-contained  --template pandoc-templates/toc-sidebarL-title.html-template.html --toc --highlight-style tango  -o ${target_file1}
 	echo "${source_file} --> ${target_file2}"
-	pandoc ${source_file} -N --toc  -o ${target_file2}
+	pandoc ${source_file} -N --toc --highlight-style tango -o ${target_file2}
 done
 
 
